e8af063c21a19f4e83546e2314196a490317ddde
Andrei <soprachev@mail.ru>
2024-06-01 01:26:21 +0300

    gtag

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+
+    <Head>
+      <Script :src="`https://www.googletagmanager.com/gtag/js?id=${gtm}`" async></Script>
+      <Script type="text/javascript" fetchpriority="high">
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', {{ gtm }});
+      </Script>
+    </Head>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -8,12 +18,12 @@
 
 
 <script setup lang="ts">
+const gtm = "G-KP1NPLTYX0"
 
 useSeoMeta({
   title: 'Позиции от WotStat',
   description: 'Мод для World of Tanks, который показывает эффективные позиции в бою. От разработчиков сервиса WotStat.',
 })
-
 
 </script>
 
